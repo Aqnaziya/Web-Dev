777664d97e4d7fd39409ae6fe74440c1ef380d73
Aqnaziya <b_aknaziya@kbtu.kz>
2025-02-23 22:27:25 +0500

    upd

--- lab 4/lab_4/src/app/products/product/product.component.ts
+++ lab 4/lab_4/src/app/products/product/product.component.ts
@@ -1,78 +1,104 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-product',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './product.component.html',
   styleUrl: './product.component.css'
 })
 export class ProductComponent {
+  categories = ['Камера', 'Гаджет', 'Конструктор', 'Техника','Раскраска'];
+  selected = 'Конструктор';
+
   products = [
     {
+      id: 1,
+      category: 'Гаджет',
+      likes: 5,
       image : 'https://resources.cdn-kaspi.kz/img/m/p/hfa/h77/86745408634910.png?format=gallery-medium', 
       name: 'Apple iPad Air 2024',
       description: 'айпадик',
       rating: '5/5',
       link: 'https://kaspi.kz/shop/p/apple-ipad-air-2024-wi-fi-11-11-djuim-8-gb-128-gb-seryi-119778165/?c=750000000'
     },
-    { image : 'https://resources.cdn-kaspi.kz/img/m/p/h47/hc5/86178022326302.jpg?format=gallery-medium',
+    { id: 2,
+      category: 'Конструктор',
+      likes: 5,
+      image : 'https://resources.cdn-kaspi.kz/img/m/p/h47/hc5/86178022326302.jpg?format=gallery-medium',
       name: 'Bricks Миньон Q5040',
       description: 'миньен',
       rating: '10/10',
       link: 'https://kaspi.kz/shop/p/bricks-min-on-q5040-detalei-180-sht-120002626/?c=750000000'
       },
-    {
+    { id: 3,
+      category: 'Гаджет',
+      likes: 5,
       image : 'https://resources.cdn-kaspi.kz/img/m/p/hcf/h69/87295489343518.png?format=gallery-medium',
       name: 'iPhone 16 Pro Max 256Gb черный',
       description: 'ипхон',
       rating: '5 /5',
       link: 'https://kaspi.kz/shop/p/apple-iphone-16-pro-max-256gb-chernyi-123787551/?c=750000000' // ссылка на товар в каспи
     },
-    {
+    { id: 4,
+      category: 'Камера',
+      likes: 5,
       image : 'https://resources.cdn-kaspi.kz/img/m/p/h09/hd9/63758980644894.jpg?format=gallery-medium',
       name: ' Canon PowerShot G7X Mark II',
       description: 'фотик мечты',
       rating: '5/5',
       link: 'https://kaspi.kz/shop/p/canon-powershot-g7x-mark-ii-chernyi-2200949/?c=750000000'
     },    
-    {
+    { id: 5,
+      category: 'Камера',
+      likes: 5,
       image : 'https://resources.cdn-kaspi.kz/img/m/p/p38/pbb/11569232.jpg?format=gallery-medium',
       name: 'Sony ZV-E10 KIT 16-50mm',
       description: 'норм фотик',
       rating: '3/5',
       link: 'https://kaspi.kz/shop/p/sony-zv-e10-kit-16-50mm-chjornyi-102510202/?c=750000000'
     },    
-    {
+    { id: 6,
+      category: 'Техника',
+      likes: 5,
       image : 'https://resources.cdn-kaspi.kz/img/m/p/h3e/h6b/83911781482526.jpg?format=gallery-medium',
       name: 'Штатив NeePho NP-3180S',
       description: 'классный штатив',
       rating: '2/5',
       link: 'https://kaspi.kz/shop/p/neepho-np-3180s-113425250/?c=750000000'
     },    
-    {
+    { id: 7,
+      category: 'Техника',
+      likes: 5,
       image : 'https://resources.cdn-kaspi.kz/img/m/p/h9f/h31/83654754533406.jpg?format=gallery-medium',
       name: 'Микрофон Boya BY-V20',
       description: 'лучшая петличка от айдарика',
       rating: '10/10',
       link: 'https://kaspi.kz/shop/p/boya-by-v20-113223542/?c=750000000'
     },    
-    {
+    { id: 8,
+      category: 'Техника',
+      likes: 5,
       image : 'https://resources.cdn-kaspi.kz/img/m/p/pea/p0f/7249416.jpg?format=gallery-medium',
       name: 'Микрофон Boya BY-M1S',
       description: 'не о чем',
       rating: '1/5',
       link: 'https://kaspi.kz/shop/p/boya-by-m1s-107651967/?c=750000000'
     },    
-    {
+    { id: 9,
+      category: 'Раскраска',
+      likes: 5,
       image : 'https://resources.cdn-kaspi.kz/img/m/p/p5a/p21/24974150.jpeg?format=gallery-medium',
       name: 'Картина Щенок в краске холст на подрамнике',
       description: 'интересное занятие',
       rating: '3/5',
       link: 'https://kaspi.kz/shop/p/schenok-v-kraske-holst-na-podramnike-20x30-sm-134744642/?c=750000000'
     },    
-    {
+    { id: 10,
+      category: 'Техника',
+      likes: 5,
       image : 'https://resources.cdn-kaspi.kz/img/m/p/pfa/p0b/24873063.jpg?format=gallery-medium',
       name: 'Клавиатура Red Square Keyrox TKL',
       description: 'клава',
@@ -82,9 +108,48 @@ export class ProductComponent {
     },
   ]
 
+  getfilteredProducts(){
+
+    var filtered = []
+    for(var i = 0; i < this.products.length;i++){
+      if(this.products[i].category === this.selected){
+        filtered.push(this.products[i]);
+      }
+    } 
+    return filtered;
+  }
+
+  selectCategory(event : any){
+    this.selected = event?.target.value;
+  }
+
+  removeProduct(id : number){
+    var newProducts = []
+    for(var i = 0; i < this.products.length;i++){
+      if(this.products[i].id !== id){
+        newProducts.push(this.products[i]);
+      }
+    }
+    this.products = newProducts;
+  }
+
+  likeProduct(id : number){
+    for(var i = 0; i < this.products.length;i++){
+      if(this.products[i].id === id){
+        this.products[i].likes += 1;
+      }
+    }
+  }
+
   share(link: string) {
     const message = encodeURIComponent(`Check this good: ${link}`);
     window.open(`https://t.me/share/url?url=${link}&text${message}`, '_blank');
+  }
+
+  selectedProduct: any = null;
+
+  selectProduct(product: any) {
+    this.selectedProduct = product;
   }
 }
 
